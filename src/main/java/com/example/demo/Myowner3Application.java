15d528095cb13fcc5ce3284dd8b02acf6d09bde1
Divyap56 <117643385+Divyap56@users.noreply.github.com>
2026-04-24 22:41:09 +0530

    intiat commit

---
+++ src/main/java/com/example/demo/Myowner3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myowner3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myowner3Application.class, args);
+	}
+
+}
